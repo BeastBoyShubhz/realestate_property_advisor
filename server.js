fdd4fb2c8fde7e45e3032607b22b3df95374e55d
Thefroghead <113317557+Thefroghead@users.noreply.github.com>
2025-08-05 16:12:50 +1000

    Changed server keys in .env and made a change in server.js removed .json after body parser

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname)));
 
 // Google Maps Key endpoint
